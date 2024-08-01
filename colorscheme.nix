bf9edb1bd3f9558026a00b6c00c884ad7dfad6cb
0x1c <kbalint@0x1c.zip>
2024-08-01 16:49:58 -0400

    Start of modifying colorscheme

--- colorscheme.nix
+++ colorscheme.nix
@@ -1,12 +1,12 @@
 {
   colors = {
-    background = "#080808";
-    foreground = "#bdbdbd";
+    background = "#000000";
+    foreground = "#dddddd";
     bold = "#eeeeee";
     cursor = "#9e9e9e";
-    cursor_text = "#080808";
+    cursor_text = "#000000";
     selection = "#b2ceee";
-    selection_text = "#080808";
+    selection_text = "#000000";
     black = "#323437";
     red = "#ff5454";
     green = "#8cc85f";
@@ -31,6 +31,21 @@
     pastel_green = "#98fb98";
     pastel_blue = "#add8e6";
     pastel_mint = "#f5fffa";
-    pastel_coral = "#ffa07a";
+    pastel_coral = "#ffb86c";
+
+    n_purple_1 = "#fbdaff";
+    n_purple_2 = "#e4c5ed";
+    n_purple_3 = "#e1c0fa";
+    n_purple_4 = "#d1a4de";
+    n_purple_5 = "#ab8cd1";
+    n_purple_6 = "#8e59cf";
+
+    n_pink_1   = "#f5c2e7";
+    n_pink_2   = "#ebbdcb";
+    n_pink_3   = "#e4a7ba";
+    n_pink_4   = "#e19cb1";
+    n_pink_5   = "#de92a9";
+    n_pink_6   = "#de7896";
+    n_pink_7   = "#db6b7c";
   };
 }
