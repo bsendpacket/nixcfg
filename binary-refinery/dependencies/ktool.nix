efe38fb62aa926fd812fb972b43345508b568ce0
0x1c <kbalint@0x1c.zip>
2024-08-06 23:08:08 -0400

    Add new dependencies for Binary Refinery

---
+++ binary-refinery/dependencies/ktool.nix
@@ -0,0 +1,31 @@
+{ lib, buildPythonPackage, fetchPypi, poetry-core, setuptools, wheel, python3Packages }:
+
+buildPythonPackage rec {
+  pname = "k2l";
+  version = "2.0.0";
+  format = "pyproject";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-66QFjxutsk7aHq4LNB+iE0tzsunhQBksakJ3YvsH11g=";
+  };
+
+  nativeBuildInputs = [
+    poetry-core
+    setuptools
+    wheel
+  ];
+
+  propagatedBuildInputs = with python3Packages; [
+    pygments
+    (callPackage ./kimg4.nix {})
+    (callPackage ./pyaes.nix {})
+  ];
+
+  meta = with lib; {
+    description = "MachO/ObjC Analysis + Editing toolkit";
+    homepage = "https://github.com/0cyn/ktool";
+    license = licenses.mit;
+    maintainers = [ "bsendpacket" ];
+  };
+}
