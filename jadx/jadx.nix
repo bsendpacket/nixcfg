aea7b6daeeac68b7b7bfd5d0556aff413902be46
0x1c <kbalint@0x1c.zip>
2024-08-08 12:19:15 -0400

    Move to newer Jadx branch by @emilytrau while waiting for it to be added to NixPkgs

--- jadx/jadx.nix
+++ jadx/jadx.nix
@@ -5,8 +5,8 @@
   gradle,
   jdk,
   quark-engine,
-  makeWrapper,
-  graphicsmagick,
+  makeBinaryWrapper,
+  imagemagick,
   makeDesktopItem,
   copyDesktopItems,
   desktopToDarwinBundle,
@@ -23,11 +23,16 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-+F+PHAd1+FmdAlQkjYDBsUYCUzKXG19ZUEorfvBUEg0=";
   };
 
+  patches = [
+    # Remove use of launch4j - contains platform binaries not able to be cached by mitmCache
+    ./no-native-deps.diff
+  ];
+
   nativeBuildInputs = [
     gradle
     jdk
-    graphicsmagick
-    makeWrapper
+    imagemagick
+    makeBinaryWrapper
     copyDesktopItems
   ] ++ lib.optionals stdenv.hostPlatform.isDarwin [ desktopToDarwinBundle ];
 
@@ -60,19 +65,13 @@ stdenv.mkDerivation (finalAttrs: {
         jadx-gui/src/main/resources/logos/jadx-logo-"$size"px.png \
         $out/share/icons/hicolor/"$size"x"$size"/apps/jadx.png
     done
-
     for size in 64 128 256; do
       mkdir -p $out/share/icons/hicolor/"$size"x"$size"/apps
-      gm convert -resize "$size"x"$size" jadx-gui/src/main/resources/logos/jadx-logo.png $out/share/icons/hicolor/"$size"x"$size"/apps/jadx.png
+      convert -resize "$size"x"$size" jadx-gui/src/main/resources/logos/jadx-logo.png $out/share/icons/hicolor/"$size"x"$size"/apps/jadx.png
     done
 
     runHook postInstall
   '';
-
-  # for size in 64 128 256; do
-  #   mkdir -p $out/share/icons/hicolor/"$size"x"$size"/apps
-  #   convert -resize "$size"x"$size" jadx-gui/src/main/resources/logos/jadx-logo.png $out/share/icons/hicolor/"$size"x"$size"/apps/jadx.png
-  # done
 
   desktopItems = [
     (makeDesktopItem {
@@ -89,7 +88,9 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   meta = with lib; {
+    changelog = "https://github.com/skylot/jadx/releases/tag/v${finalAttrs.version}";
     description = "Dex to Java decompiler";
+    homepage = "https://github.com/skylot/jadx";
     longDescription = ''
       Command line and GUI tools for produce Java source code from Android Dex
       and Apk files.
