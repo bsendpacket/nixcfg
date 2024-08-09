2cf29f3c5c947c090767d8bb9342c7addd1c451b
0x1c <kbalint@0x1c.zip>
2024-08-09 18:30:09 -0400

    Add donut-decryptor

---
+++ donut-decryptor/dependencies/chaskey-lts.nix
@@ -0,0 +1,23 @@
+{ python3Packages, fetchFromGitHub }:
+
+python3Packages.buildPythonPackage {
+  pname = "chaskey-lts";
+  version = "0.0.3";
+
+  src = fetchFromGitHub {
+    owner = "volexity";
+    repo = "chaskey-lts";
+    rev = "66d75bfa1d32fdac9508e710a9ade234690463bb";
+    hash = "sha256-KFMpHSc21V1divx8vUGjHzmoz9ZFcyBgVnK6pmtz7OM=";
+  };
+
+  nativeBuildInputs = with python3Packages; [
+    setuptools
+  ];
+
+  meta = {
+    description = "A pure Python chaskey cipher implementation developed initially for use with the donut_decryptor.";
+    homepage = "https://github.com/volexity/chaskey-lts";
+    maintainers = [ "bsendpacket" ];
+  };
+}
---
+++ donut-decryptor/dependencies/lznt1.nix
@@ -0,0 +1,31 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, setuptools
+, wheel
+}:
+
+buildPythonPackage rec {
+  pname = "lznt1";
+  version = "0.2";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-/cWbV9RiADA41XKxfhZao8j5GjPfaJSKNElpoolrwuo=";
+  };
+
+  nativeBuildInputs = [
+    setuptools
+    wheel
+  ];
+
+  pythonImportsCheck = [ "lznt1" ];
+
+  meta = with lib; {
+    description = "Python module for LZNT1 compression/decompression";
+    homepage = "https://pypi.org/project/lznt1/";
+    license = licenses.unfree; # FIXME: nix-init did not found a license
+    maintainers = with maintainers; [ ];
+  };
+}
