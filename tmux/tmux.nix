4d21ce632c7b61ed35e99540c5e62e27595aadd0
0x1c <kbalint@0x1c.zip>
2024-09-11 21:16:04 -0400

    Update tmux config

--- tmux/tmux.nix
+++ tmux/tmux.nix
@@ -8,14 +8,22 @@
     customPaneNavigationAndResize = true;
 
     extraConfig = ''
-      set -g status-style bg=default
-      set -g default-terminal "tmux-256color"
-      set -as terminal-overrides ',xterm*:Tc:smxx=\E[9m:rmxx=\E[23m'
-
+      # Vim style bindings for resizing
       bind -n C-h resize-pane -L 2   # Hold Ctrl + h to resize left
       bind -n C-j resize-pane -D 2   # Hold Ctrl + j to resize down
       bind -n C-k resize-pane -U 2   # Hold Ctrl + k to resize up
       bind -n C-l resize-pane -R 2   # Hold Ctrl + l to resize right
+
+      # Toggle status bar with C-b Space
+      bind Space set-option status
+
+      # Start from 1 instead of 0
+      set-option -g base-index 1
+
+      set -g status-style bg=default
+      set -g default-terminal "tmux-256color"
+      set -as terminal-overrides ',xterm*:Tc:smxx=\E[9m:rmxx=\E[23m'
+
     '';
   };
 }
