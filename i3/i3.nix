d0f6e8ab9e1c9c32ced6e2a656e15a9400165189
0x1c <kbalint@0x1c.zip>
2024-12-13 14:28:07 -0500

    Update i3/tmux handling system

--- i3/i3.nix
+++ i3/i3.nix
@@ -1,6 +1,33 @@
 { channels, config, lib, nixGLPrefix, ... }: 
 let 
+  tmuxWrapper = channels.nixpkgs-unstable.writeShellScriptBin "tmux-main" ''
+    #!${channels.nixpkgs-unstable.bash}/bin/bash
+    if ! ${channels.nixpkgs-unstable.tmux}/bin/tmux -2 has-session -t main 2>/dev/null; then
+      ${channels.nixpkgs-unstable.tmux}/bin/tmux -2 new-session -d -s main
+    fi
+    exec ${channels.nixpkgs-unstable.tmux}/bin/tmux -2 attach -t main
+  '';
+
+  scratchWrapper = channels.nixpkgs-unstable.writeShellScriptBin "tmux-scratch" ''
+    #!${channels.nixpkgs-unstable.bash}/bin/bash
+    if ! ${channels.nixpkgs-unstable.tmux}/bin/tmux -2 has-session -t scratch 2>/dev/null; then
+      ${channels.nixpkgs-unstable.tmux}/bin/tmux -2 new-session -d -s scratch
+    fi
+    exec ${nixGLPrefix}${channels.nixpkgs-unstable.contour}/bin/contour --class scratchpad ${channels.nixpkgs-unstable.tmux}/bin/tmux -2 attach -t scratch
+  '';
+
   modifier = "Mod1";
+  # Main terminal
+  terminal = "${nixGLPrefix}${channels.nixpkgs-unstable.contour}/bin/contour ${tmuxWrapper}/bin/tmux-main";
+
+  # Scratch terminal (Just to quickly run some commands)
+  scratchTerminal = "${scratchWrapper}/bin/tmux-scratch";
+
+  # Create a new tmux session
+  newSessionCmd = ''${nixGLPrefix}${channels.nixpkgs-unstable.contour}/bin/contour ${channels.nixpkgs-unstable.tmux}/bin/tmux -2 new-session -s'';
+
+  # Open a terminal with a given tmux session
+  attachSessionCmd = ''${nixGLPrefix}${channels.nixpkgs-unstable.contour}/bin/contour ${channels.nixpkgs-unstable.tmux}/bin/tmux -2 new-window -t'';
 in {
   xsession = {
     windowManager.i3 = {
@@ -11,7 +38,8 @@ in {
 
         defaultWorkspace = "workspace number 1";
 
-        terminal = "${nixGLPrefix}${channels.nixpkgs-unstable.contour}/bin/contour ${channels.nixpkgs-unstable.tmux}/bin/tmux";
+        # Mod+Enter opens the terminal under the 'main' tmux session
+        terminal = terminal;
 
         bars = [{ 
           command = "${channels.nixpkgs-unstable.i3}/bin/i3bar";
@@ -38,7 +66,17 @@ in {
         # TIP: Utilizing `lib.mkOptionDefault` here allows us to keep all the defaults
         # and simply add new keybinds to the configuration.
         keybindings = lib.mkOptionDefault {
-          
+
+          # Create new tmux session
+          "${modifier}+Shift+Return" = "exec ${channels.nixpkgs-unstable.rofi}/bin/rofi -dmenu -p 'New Session Name' | xargs -r ${newSessionCmd}";
+
+          # Attach to existing tmux session
+          "${modifier}+s" = "exec ${channels.nixpkgs-unstable.tmux}/bin/tmux list-sessions | ${channels.nixpkgs-unstable.rofi}/bin/rofi -i -dmenu | cut -d: -f1 | xargs -r ${attachSessionCmd}";
+
+          # Attach to the scratch terminal
+          #"${modifier}+Escape" = "exec --no-startup-id ${scratchTerminal}; exec --no-startup-id i3-msg floating enable, resize set 1200 700, move position center";
+          "${modifier}+Escape" = "[class=scratchpad] scratchpad show";
+
           # Vim-like keybindings for i3
           "${modifier}+h" = "focus left";
           "${modifier}+j" = "focus down";
@@ -57,7 +95,7 @@ in {
           "${modifier}+Shift+w" = "exec --no-startup-id ${channels.nixpkgs-unstable.zsh}/bin/zsh -c 'LANG=en_US.UTF-8 LC_ALL=C ${channels.nixpkgs-unstable.rofi}/bin/rofi -show window'";
 
           # Floating window toggle
-          "${modifier}+Escape" = "scratchpad show";
+          #"${modifier}+Escape" = "scratchpad show";
           "${modifier}+space" = "floating toggle";
 
           # Laptop function keys
@@ -70,6 +108,16 @@ in {
       };
 
       extraConfig = ''
+        # Launch the scratchpad at logon
+        exec --no-startup-id ${scratchTerminal}
+
+        # Configure scratchpad window
+        for_window [class="scratchpad"] border none
+        for_window [class="scratchpad"] floating enable
+        for_window [class="scratchpad"] resize set 1200 700
+        for_window [class="scratchpad"] move position center
+        for_window [class="scratchpad"] move scratchpad
+
         mode "resize" {
           bindsym Up resize shrink height 10 px or 10 ppt
           bindsym Down resize grow height 10 px or 10 ppt
@@ -92,7 +140,6 @@ in {
         exec --no-startup-id nm-applet
         exec --no-startup-id vmware-user
         exec --no-startup-id vmware-user-suid-wrapper
-
       '';
     };
   };
