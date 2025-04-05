8d0b096493234649c967d0e2eaeb435e11b667c9
0x1c <kbalint@0x1c.zip>
2025-04-05 14:15:49 -0400

    Update Obfuscator-LLVM demo

--- obfuscator-llvm/demo/demo.c
+++ obfuscator-llvm/demo/demo.c
@@ -1,26 +1,56 @@
 #include <stdio.h>
 
-static inline int modify(int input) {
-    int val_1 = input * 2;
-    int val_2 = input + 2;
+__attribute__((noinline))
+int add_op(int a, int b) { return a + b; }
 
-    return (val_1 ^ val_2);
+__attribute__((noinline))
+int sub_op(int a, int b) { return a - b; }
+
+__attribute__((noinline))
+int mul_op(int a, int b) { return a * b; }
+
+__attribute__((noinline))
+int xor_op(int a, int b) { return a ^ b; }
+
+__attribute__((noinline))
+int and_op(int a, int b) { return a & b; }
+
+__attribute__((noinline))
+int or_op(int a, int b) { return a | b; }
+
+__attribute__((noinline))
+void transform_array(int* input, int size) {
+    printf("[*] Array Transform: Start\n");
+    
+    for (int i = 0; i < size; i++) {
+        input[i] = add_op(input[i], 3);
+        printf("[*] Add | Current Value: %i\n", input[i]);
+        
+        input[i] = sub_op(input[i], 3);
+        printf("[*] Sub | Current Value: %i\n", input[i]);
+        
+        input[i] = and_op(input[i], 3);
+        printf("[*] AND | Current Value: %i\n", input[i]);
+        
+        input[i] = or_op(input[i], 3);
+        printf("[*] OR | Current Value: %i\n", input[i]);
+        
+        input[i] = xor_op(input[i], 3);
+        printf("[*] XOR | Current Value: %i\n", input[i]);
+    }
+    
+    printf("[*] Array Transform: End\n");
 }
 
 int main() {
-    int data[0x10];
-
-    for (int i = 0; i < 0x10; i++) {
-        data[i] = modify(i);
+    int test[5] = {1, 2, 3, 4, 5};
+    transform_array(test, 5);
+    
+    printf("Transformed array: ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", test[i]);
     }
-
-    for (int i = 0; i < 0x10; i++) {
-        if (data[i] % 2 == 0) {
-            printf("%i is even\n", data[i]);
-        } else {
-            printf("%i is odd\n", data[i]);
-        }
-    }
-
+    printf("\n");
+    
     return 0;
 }
